7c91d79938e082326b78c49d538ac0a041068c79
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-07-10 13:17:43 +0300

    done with stream

--- app/chat/_layout.tsx
+++ app/chat/_layout.tsx
@@ -1,33 +1,42 @@
 import { Stack } from "expo-router";
-import {useEffect} from "react";
-import { StreamChat } from 'stream-chat';
-
-
-
+import { useEffect } from "react";
+import { StreamChat } from "stream-chat";
 
 const API_KEY = "vjepn8rkwv8t";
 const client = StreamChat.getInstance(API_KEY);
 
-
-export default function ChatLayout() { 
-useEffect(() => {
-//  connset the user
+export default function ChatLayout() {
+    useEffect(() => {
+      
+        
+    //  connset the user
     const connectUser = async () => {
-        await client.connectUser({
+      await client.connectUser(
+        {
           id: "ievgen",
           name: "Ievgen",
-          image: "https://i.imgur.com/fR9Jz14.png"
+          image: "https://i.imgur.com/fR9Jz14.png",
         },
-            client.devToken("ievgen")
-        )
-    }
-    connectUser();   
-}, []);
+        client.devToken("ievgen")
+      );
+        
+        
+      // create a channel
+      const channel = client.channel("livestream", "public", {
+        name: "Public",
+      });
+        await channel.create();
+    };
+    connectUser();
 
+    return () => {
+      client.disconnectUser();
+    };
+  }, []);
 
-    return (
-      <Stack>
-            <Stack.Screen name='index' options={ { title: 'Messages'}} />
-      </Stack>
-    );
-} 
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Messages" }} />
+    </Stack>
+  );
+}
